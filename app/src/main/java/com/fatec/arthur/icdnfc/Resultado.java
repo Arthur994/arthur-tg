9b2fe59993220f6123bd925b5277093b2ac16a45
Arthur994 <30422698+Arthur994@users.noreply.github.com>
2017-08-09 16:21:43 -0300

    Busca/exibição na tela de resultado
    feita a busca e exibição dos resultados em tela.
    Falta personalizar a exibição pois está visualmente ruim.

--- app/src/main/java/com/fatec/arthur/icdnfc/Resultado.java
+++ app/src/main/java/com/fatec/arthur/icdnfc/Resultado.java
@@ -3,19 +3,18 @@ package com.fatec.arthur.icdnfc;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.fatec.arthur.icdnfc.database.BancoDados;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Resultado extends AppCompatActivity {
 
-    private BancoDados bd;
-
-    // TODO: 08/08/2017 Receber lista de string com os codigos; 
-
-    // TODO: 08/08/2017 manipular e fazer a busca
-
-    // TODO: 08/08/2017 Exibir resultados
+    private BancoDados bd = new BancoDados(this);
 
 /*
     public String ToStringCap(Cursor curcap){
@@ -31,6 +30,29 @@ public class Resultado extends AppCompatActivity {
                 "Nome: " + curcod.getString(8);
     }
 */
+    // TODO: 08/08/2017 Receber lista de string com os codigos;
+
+    // TODO: 08/08/2017 manipular e fazer a busca
+    public List<String> busca(List<String> lista){
+
+        List<String> resultado = new LinkedList<String>();
+
+        for(String c:lista){
+            Cursor cod = bd.selecionarCodByID(c);
+            Cursor block = bd.selecionarBlocoByID(cod.getString(4));
+            Cursor cap = bd.selecionarCapByID(Integer.parseInt(cod.getString(3)));
+            
+            String aux = ToString(cod,cap,block);
+            resultado.add(aux);
+
+            //System.out.println("teste: " + cod.getString(4));
+
+        }
+        return resultado;
+    }
+
+    // TODO: 08/08/2017 Exibir resultados
+
 
      public String ToString(Cursor curcod,Cursor curcap,Cursor curblock){
          return "CID: " + curcod.getString(7) +
@@ -45,6 +67,17 @@ public class Resultado extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
+
         ListView resultados = (ListView) findViewById(R.id.resultado);
+
+        //teste
+        List<String> teste = new ArrayList<String>();
+        teste.add("A00");
+        teste.add("B182");
+
+        //busca(teste);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, busca(teste));
+        resultados.setAdapter(adapter);
     }
 }
